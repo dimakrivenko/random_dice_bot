71a1cd106e646ad9e9ab87b3d41a0a6bcdcea383
dimakrivenko <wen2333@gmial.com>
2018-04-09 16:06:41 +0300

    fix

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-const Router = require('koa-router');
+const express = require('express');
 
-const app = new Koa();
-const router = Router();
+const app = express();
 
 
-router.get('/', ctx => {
+app.get('/', function(req, res) {
 	console.log('route /')
-	ctx.body = 'Random Dice Bot'
+	res.send('Random Dice Bot')
 });
 
 
 
-app.use(bodyParser())
-app.use(router.routes())
-
 const serverPort = process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || process.env.WEB_PORT || 8080;
 const serverIp = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || process.env.IP || '127.0.0.1';
 
-app.listen(serverPort, serverIp, () => {
+app.listen(serverPort, serverIp, function() {
 	console.log('Server listening on ' + serverIp + ':' + serverPort);
 });
 
